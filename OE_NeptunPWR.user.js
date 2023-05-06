ea4bebb7d266a53eca43f410543483e4a574a842
Zárai László Levente <115075511+zarailaszlo@users.noreply.github.com>
2023-05-06 22:47:25 +0200

    downloadURL

--- OE_NeptunPWR.user.js
+++ OE_NeptunPWR.user.js
@@ -8,6 +8,7 @@
 // @supportURL   https://github.com/zarailaszlo/OE_NeptunPWR/
 // @namespace   https://github.com/zarailaszlo/OE_NeptunPWR/
 // @updateURL   https://github.com/zarailaszlo/OE_NeptunPWR/raw/main/OE_NeptunPWR.user.js
+// @downloadURL https://github.com/zarailaszlo/OE_NeptunPWR/raw/main/OE_NeptunPWR.user.js
 // @grant        none
 // ==/UserScript==
 
